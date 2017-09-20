a23874d2907711bb9ad40340c12e8bf25b558934
DESKTOP-LUDQI20\gterra <gterra@terradossoftwares.com>
2017-09-20 16:05:38 -0300

    Direciona automaticamente para tela de login.

--- projeto_smge/dashboard/urls.py
+++ projeto_smge/dashboard/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.do_login, name='login'),
 	
 	url(r'^graficos$', views.graficos, name='graficos'),
     url(r'^login$', views.do_login, name='login'),
