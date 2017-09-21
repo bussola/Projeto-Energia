54c71c5a5f22aeabc65364a11b13d2ceb9488d64
bussola <vbussola@yahoo.com>
2017-09-21 10:52:53 +0000

    resolvido problema do css do admin

--- projeto_smge/projeto_smge/settings.py
+++ projeto_smge/projeto_smge/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'NAME': 'shero',
         'USER': 'postgres',
         'PASSWORD': 'fuvest',
-        'HOST': 'localhost',   # url produção: projeto_sge.mysql.dbaas.com.br (186.202.152.190)
+        'HOST': 'localhost',
         'PORT': '5432',
     }
 }
@@ -122,8 +122,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/dashboard/static/'
 
 LOGIN_URL = '/login'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'dashboard/static/')
