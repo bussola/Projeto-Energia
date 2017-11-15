973a8f938c079f20372329ae8cbcf6965f6f7eb8
bussola <vbussola@yahoo.com>
2017-11-15 20:29:33 -0200

    alteracao_password_10

--- Projeto-Energia/projeto_smge/dashboard/urls.py
+++ Projeto-Energia/projeto_smge/dashboard/urls.py
@@ -15,11 +15,16 @@ urlpatterns = [
 
     url(r'^password$', views.do_change_password, name='change_password'),
 
-    url(r'^password_reset/$', auth_views.password_reset, name='password_reset'),
-    url(r'^password_reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
+    # url(r'^password_reset/$', auth_views.password_reset, name='password_reset'),
+
+    url(r'^password_reset$', auth_views.password_reset, {
+        'post_reset_redirect': '/password_reset/done/'
+    }, name='password_reset'),
+
+    url(r'^password_reset/done$', auth_views.password_reset_done, name='password_reset_done'),
     url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
         auth_views.password_reset_confirm, name='password_reset_confirm'),
-    url(r'^reset/done/$', auth_views.password_reset_complete, name='password_reset_complete'),
+    url(r'^reset/done$', auth_views.password_reset_complete, name='password_reset_complete'),
 
 
 	url(r'^graficos$', views.graficos, name='graficos'),
