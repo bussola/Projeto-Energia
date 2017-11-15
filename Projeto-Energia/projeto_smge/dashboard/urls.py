c85c2d5a4d0dbf399ea5831f656275510fa64881
bussola <vbussola@yahoo.com>
2017-11-15 14:28:53 -0200

    alteracao_password_9

--- Projeto-Energia/projeto_smge/dashboard/urls.py
+++ Projeto-Energia/projeto_smge/dashboard/urls.py
@@ -14,9 +14,11 @@ urlpatterns = [
 
     url(r'^password$', views.do_change_password, name='change_password'),
 
-    url(r'^reset/confirm/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)$',
-            views.reset_confirm, name='reset_confirm'),
-    url(r'^reset$', views.reset, name='reset'),
+    url(r'^password_reset/$', auth_views.password_reset, name='password_reset'),
+    url(r'^password_reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        auth_views.password_reset_confirm, name='password_reset_confirm'),
+    url(r'^reset/done/$', auth_views.password_reset_complete, name='password_reset_complete'),
 
 
 	url(r'^graficos$', views.graficos, name='graficos'),
--- Projeto-Energia/projeto_smge/projeto_smge/settings.py
+++ Projeto-Energia/projeto_smge/projeto_smge/settings.py
@@ -69,7 +69,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'projeto_smge.wsgi.application'
-
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
