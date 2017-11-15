cef69cbc9a4a00c1230af097500040080d6edb1c
bussola <vbussola@yahoo.com>
2017-11-15 20:41:43 -0200

    alteracao_password_10

--- Projeto-Energia/projeto_smge/dashboard/urls.py
+++ Projeto-Energia/projeto_smge/dashboard/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 from . import views
-from django.contrib.auth import views as auth_views
+from django.contrib.auth_views import views as auth_views
 
 handler400 = 'views.my_custom_bad_request_view'
 handler403 = 'views.my_custom_permission_denied_view'
@@ -15,12 +15,11 @@ urlpatterns = [
 
     url(r'^password$', views.do_change_password, name='change_password'),
 
-    url(r'^password_reset/$', auth_views.password_reset, name='password_reset'),
-
     # url(r'^password_reset$', auth_views.password_reset, {
     #     'post_reset_redirect': 'dashboard/password_reset/done/'
     # }, name='password_reset'),
 
+    url(r'^password_reset/$', auth_views.password_reset, name='password_reset'),
     url(r'^password_reset/done$', auth_views.password_reset_done, name='password_reset_done'),
     url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
         auth_views.password_reset_confirm, name='password_reset_confirm'),
