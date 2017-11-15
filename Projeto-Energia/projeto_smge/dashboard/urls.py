53cbaa7cebb34067c7278c6aedcc62c18793ece6
bussola <vbussola@yahoo.com>
2017-11-15 20:35:30 -0200

    alteracao_password_10

--- Projeto-Energia/projeto_smge/dashboard/urls.py
+++ Projeto-Energia/projeto_smge/dashboard/urls.py
@@ -18,7 +18,7 @@ urlpatterns = [
     # url(r'^password_reset/$', auth_views.password_reset, name='password_reset'),
 
     url(r'^password_reset$', auth_views.password_reset, {
-        'post_reset_redirect': '/password_reset/done/'
+        'post_reset_redirect': 'dashboard/password_reset/done/'
     }, name='password_reset'),
 
     url(r'^password_reset/done$', auth_views.password_reset_done, name='password_reset_done'),
