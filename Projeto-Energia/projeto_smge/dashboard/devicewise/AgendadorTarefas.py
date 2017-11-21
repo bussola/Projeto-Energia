52af5999c5fabc0aca8efbed30c18ed1dd7cb3a4
Gilberto B. Terra Jr <gterra@terradossoftwares.com>
2017-11-21 14:21:10 -0200

    Agendador de Tarefas que será usado para agendar todas as tarefas necessárias.

---
+++ Projeto-Energia/projeto_smge/dashboard/devicewise/AgendadorTarefas.py
@@ -0,0 +1,20 @@
+from dashboard.devicewise.DevicewiseColetor import DevicewiseColetor
+from dashboard.models import User
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+sched = BlockingScheduler()
+
+@sched.scheduled_job('interval', seconds=3)
+def coletar_tudo():
+    print('------------------------------------------')
+    print('Sincronizando com API (transdutores)')
+    api = DevicewiseColetor()
+    for u in User.objects.all():
+        api.set_usuario(u)
+        print('Coletando dados do usuário "%s"' % u.email)
+        if not api.coletar_por_usuario():
+            print('Não foi possível coletar dados do usuário "%s"' % u.email)
+
+    print('Todas as leituras foram coletadas com sucesso.')
+
+sched.start()
