85af3639110a07c04a2d7ae94d6d011d4678a825
bussola <vbussola@yahoo.com>
2017-11-21 16:07:13 +0000

    install apscheduler

--- Projeto-Energia/projeto_smge/dashboard/management/commands/comando.py
+++ Projeto-Energia/projeto_smge/dashboard/management/commands/comando.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand, CommandError
-from dashboard.devicewise.DevicewiseHttp import DevicewiseHttp
+#from dashboard.devicewise.DevicewiseHttp import DevicewiseHttp
 from dashboard.devicewise.DevicewiseColetor import DevicewiseColetor
 
 class Command(BaseCommand):
