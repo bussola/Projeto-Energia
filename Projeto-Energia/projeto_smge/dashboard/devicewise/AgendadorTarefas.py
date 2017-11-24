fb22ab59003d597e35638b205ccf28c62fd22473
Gilberto B. Terra Jr <gterra@terradossoftwares.com>
2017-11-24 14:22:02 -0200

    Correcoes agendador tarefas

--- Projeto-Energia/projeto_smge/dashboard/devicewise/AgendadorTarefas.py
+++ Projeto-Energia/projeto_smge/dashboard/devicewise/AgendadorTarefas.py
@@ -9,16 +9,11 @@ sched = BlockingScheduler()
 
 @sched.scheduled_job('interval', seconds=10, max_instances=100)
 def coletar_tudo():
+    agora = str(datetime.datetime.today())
     print('\n%s ]----------------------------------------------------------------------' % agora)
     coletor_api = DevicewiseColetor()
-    agora = str(datetime.datetime.today())
-<<<<<<< HEAD
     usuarios = User.objects.all()
     for u in usuarios:
-=======
-    print('\n%s ]----------------------------------------------------------------------' % agora)
-    for u in User.objects.all():
->>>>>>> d4109232d1e827e52349e109a4a7c7e6ae85bc89
         print('Sincronizando com usuário %s' % u.email)
         if not coletor_api.coletar_por_usuario(u):
             print('Agendador : Não foi possível coletar dados do usuário "%s"' % u.email)
