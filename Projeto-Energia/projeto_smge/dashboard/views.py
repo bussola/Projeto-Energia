1642edafc0c127ae8d28d5cdc5510add3d0d3231
bussola <vbussola@yahoo.com>
2017-11-29 19:27:21 -0200

    printa

--- Projeto-Energia/projeto_smge/dashboard/views.py
+++ Projeto-Energia/projeto_smge/dashboard/views.py
@@ -2,6 +2,7 @@ from django.http import Http404
 from django.http import HttpResponse
 from django.http import JsonResponse
 from django.contrib import messages
+from django.db.models import Count
 from django.contrib.auth import update_session_auth_hash
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
