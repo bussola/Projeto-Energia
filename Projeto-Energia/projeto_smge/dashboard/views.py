5df433bcaeafac3c25ba83c9125f768999c07abc
bussola <vbussola@yahoo.com>
2017-11-29 19:52:45 -0200

    printa

--- Projeto-Energia/projeto_smge/dashboard/views.py
+++ Projeto-Energia/projeto_smge/dashboard/views.py
@@ -15,7 +15,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from dashboard.devicewise.DevicewiseHttp import DevicewiseHttp
 from dashboard.devicewise.DevicewiseColetor import DevicewiseColetor
-import datetime
+from datetime import datetime
 from datetime import timedelta
 
 from django.core.urlresolvers import reverse
