f29fd083f0969d489ab952703f6bddfe351ecded
bussola <vbussola@yahoo.com>
2017-11-30 21:00:53 -0200

    .

--- Projeto-Energia/projeto_smge/dashboard/views.py
+++ Projeto-Energia/projeto_smge/dashboard/views.py
@@ -98,7 +98,7 @@ def printa(request):
     .extra(select={'day': 'date(data_leitura)'})
     .values('day')
     .annotate(contador=Count('io6')))
-    qnt = qnt_dados[1]
+    qnt = qnt_dados.first()
 
     coletas = Coleta.objects.all().order_by('-id')
     transdutores = Transdutor.objects.filter(chave_api="hab0001")
