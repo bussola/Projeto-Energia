362c09c529f6b52014401c719442522f007ef602
bussola <vbussola@yahoo.com>
2017-11-30 23:35:36 -0200

    .

--- Projeto-Energia/projeto_smge/dashboard/views.py
+++ Projeto-Energia/projeto_smge/dashboard/views.py
@@ -92,7 +92,8 @@ def printa(request):
     .extra(select={'day': 'date(data_leitura)'})
     .values('day')
     .annotate(sum=Sum('io6')))
-    filtro = soma_5_min
+    for f in soma_5_min:
+        filtro = f['sum']
 
     qnt_dados = (Coleta.objects
     .filter(data_leitura__gt=last_5_min)
