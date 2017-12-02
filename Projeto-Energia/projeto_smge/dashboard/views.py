be217215f9c1a8d3a0169e9f10bc618d388edb4e
bussola <vbussola@yahoo.com>
2017-12-02 13:47:37 -0200

    tabela coleta - coluna media io6

--- Projeto-Energia/projeto_smge/dashboard/views.py
+++ Projeto-Energia/projeto_smge/dashboard/views.py
@@ -112,6 +112,7 @@ def printa(request):
     coletas = Coleta.objects.all().order_by('-id')
     transdutores = Transdutor.objects.filter(chave_api="hab0001")
     io6_name = Transdutor.objects.values_list('nome_io6', flat=True).filter(chave_api="hab0001")
+    
     parametros = Transdutor.objects.all().filter(chave_api="hab0001").order_by('-id')
     parametro_a = Transdutor.objects.values_list('parametro_a', flat=True).filter(chave_api="hab0001").order_by('-id').first()
     parametro_b = Transdutor.objects.values_list('parametro_b', flat=True).filter(chave_api="hab0001").order_by('-id').first()
